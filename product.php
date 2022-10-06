2e97e54f17f4953920ae5d652a107ede6a4c0939
unknown <kyomuhendoisihaka1@gmail.com>
2022-10-06 04:54:28 -0400

    h

--- product.php
+++ product.php
@@ -1,5 +1,17 @@
 <?php include("base.php") ?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
 <style>
     img{
         width:250px;
@@ -220,3 +232,6 @@
         </div>
     </div>
 </div>
+</body>
+</html>
+
